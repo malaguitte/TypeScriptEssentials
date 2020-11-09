0e077445c0724ab1234203123f267f891234584e
Anderson <upmalagutti@gmail.com>
2020-11-09 21:15:50 +0100

    fixed typo on the html

--- Chapter_02/02_03/app.ts
+++ Chapter_02/02_03/app.ts
@@ -10,7 +10,7 @@ const displayName = `Todo #${todo.id}`;
 
 
 container.innerHTML = `
-<div todo='${todo.id}' class="list-group-item}">
+<div todo='${todo.id}' class="list-group-item">
     <i class="${todo.completed ? "" : "hidden"}" text-success glyphicon glyphicon-ok"></i>
     <span class="name">${displayName}</span>
 </div>`;
