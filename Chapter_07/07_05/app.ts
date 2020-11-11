2a34f20f03c881be3d5733ab305a87c4a31380a6
Anderson <upmalagutti@gmail.com>
2020-11-11 20:23:04 +0100

    Examples of how to import modules

---
+++ Chapter_07/07_05/app.ts
@@ -0,0 +1,11 @@
+//Importing Todo from model
+import { Todo } from "./model";
+
+//Importing both Todo and TodoState from model
+// import { Todo, TodoState } from "./model";
+
+//Importing but using a custom name
+// import { Todo as TodoTask } from "./model";
+
+
+let someTodo: Todo;
