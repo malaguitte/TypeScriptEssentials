97cda0bc45beb17a82d3ea2257094eb82f49334c
Anderson <upmalagutti@gmail.com>
2020-11-13 10:50:19 +0100

    Method decorators example

---
+++ Chapter_09/09_01/TodoApp.ts
@@ -0,0 +1,59 @@
+import { Todo, TodoState } from "./Model";
+import TodoService, { ITodoService } from "./TodoService"
+import TodoListComponent from "./TodoListComponent";
+
+export class TodoApp {
+    private todoService: ITodoService;
+    private todoList: TodoListComponent;
+
+    constructor(el: HTMLElement, todos: string[]) {
+        this.todoService = new TodoService(todos);
+        this.initialize(el);
+    }
+
+    addTodo(todoName) {
+        this.todoService.add(todoName);
+        this.renderTodos();
+    }
+
+    clearCompleted() {
+        this.todoService.clearCompleted();
+        this.renderTodos();
+    }
+
+    toggleTodoState(todoId) {
+        this.todoService.toggle(todoId);
+        this.renderTodos();
+    }
+
+    renderTodos() {
+        const todos: Todo[] = this.todoService.getAll();
+        this.todoList.render(todos);
+    }
+
+    initialize(el) {
+        const addTodoFormEl = el.getElementsByClassName("add-todo")[0];
+        const addTodoNameEl = addTodoFormEl.getElementsByTagName("input")[0];
+        const todoListEl = el.getElementsByClassName("todo-list")[0];
+        const clearCompletedEl = el.getElementsByClassName("clear-completed")[0];
+
+        addTodoFormEl.addEventListener("submit", (event) => {
+            this.addTodo(addTodoNameEl.value);
+            addTodoNameEl.value = "";
+            event.preventDefault();
+        });
+
+        todoListEl.addEventListener("todo-toggle", (event) => {
+            const todoId = event.detail.todoId;
+            this.todoService.toggle(todoId);
+            this.renderTodos();
+        });
+
+        clearCompletedEl.addEventListener("click", () => this.clearCompleted());
+
+        this.todoList = new TodoListComponent(todoListEl);
+
+        this.renderTodos();
+    }
+
+}
---
+++ Chapter_09/09_01/TodoListComponent.ts
@@ -0,0 +1,52 @@
+import { Todo, TodoState } from "./Model";
+import "https://code.jquery.com/jquery-1.12.1.min.js";
+
+export default class TodoListComponent {
+    private $el: JQuery;
+    
+    constructor(el: HTMLElement) {
+        this.$el = $(el);
+    }
+
+    render(todos) {
+        this.$el.html("");
+        
+        if (!todos.length) {
+            const content = `
+            <div class='list-group-item text-center text-giant'>
+                <strong>You've completed everything you needed to do!</strong>
+            </div>`;
+
+            this.$el.html(content);
+            return;
+        }
+
+        todos.forEach(todo => {
+            this.renderTodo(todo).appendTo(this.$el);
+        });
+    }
+
+    private renderTodo(todo) {
+        const content = `
+        <div class='todo-item list-group-item ${(todo.state === TodoState.Completed ? 'completed' : '')}'>
+            <div class='row'>
+                <div class='col-md-2 text-center'>
+                    <i class='incomplete glyphicon glyphicon-unchecked text-muted text-giant'></i>
+                    <i class='completed-indicator completed glyphicon glyphicon-ok text-giant'></i>
+                </div>
+                <div class='col-md-10'>
+                    <span class='incomplete text-giant'>${todo.name}</span>
+                    <span class='completed text-strikethrough text-muted text-giant'>${todo.name}</span>
+                </div>
+            </div>
+            <div class='clearfix'></div>
+        </div>`;
+        const onClick = function() {
+            const event = document.createEvent("CustomEvent");
+            event.initCustomEvent("todo-toggle", true, true, { todoId: todo.id });
+            this.dispatchEvent(event);
+        };
+
+        return $(content).on("click", onClick);
+    }
+}
